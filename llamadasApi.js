b74156813f01b2c42730faa5e3d479bbc919c21f
Albert Araque <aaraquedesarrollo@gmail.com>
2021-07-05 18:40:44 +0200

    refactorizado y acabado de verdad

--- llamadasApi.js
+++ llamadasApi.js
@@ -2,19 +2,19 @@ const fetch = require("node-fetch");
 require("dotenv").config();
 
 const urlAPI = process.env.URL_API;
-const paradasEndPoint = process.env.PARADAS_ENDPOINT
+const paradasEndPoint = process.env.PARADAS_ENDPOINT;
 const apiAuth = `?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
 
-
 const obtenerLineas = async () => {
-const resp = await fetch(`${urlAPI}${apiAuth}`);
+  const resp = await fetch(`${urlAPI}${apiAuth}`);
   const { features } = await resp.json();
   return features;
-}
+};
 
 const obtenerParadasLinea = async (codiLinia) => {
-  console.log(`${paradasEndPoint}${codiLinia}/estacions${apiAuth}`);
-  const resp = await fetch(`${paradasEndPoint}${codiLinia}/estacions${apiAuth}`);
+  const resp = await fetch(
+    `${paradasEndPoint}${codiLinia}/estacions${apiAuth}`
+  );
   if (resp.ok) {
     const paradas = await resp.json();
     return paradas;
@@ -23,5 +23,5 @@ const obtenerParadasLinea = async (codiLinia) => {
 
 module.exports = {
   obtenerLineas,
-  obtenerParadasLinea
-}
+  obtenerParadasLinea,
+};
