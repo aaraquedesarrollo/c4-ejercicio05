17799c0a943ad2bc6a7a30c82b41f18083cfd25b
Albert Araque <aaraquedesarrollo@gmail.com>
2021-07-05 18:13:28 +0200

    Ejercicio competado

---
+++ llamadasApi.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+const urlAPI = process.env.URL_API;
+const paradasEndPoint = process.env.PARADAS_ENDPOINT
+const apiAuth = `?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
+
+
+const obtenerLineas = async () => {
+const resp = await fetch(`${urlAPI}${apiAuth}`);
+  const { features } = await resp.json();
+  return features;
+}
+
+const obtenerParadasLinea = async (codiLinia) => {
+  console.log(`${paradasEndPoint}${codiLinia}/estacions${apiAuth}`);
+  const resp = await fetch(`${paradasEndPoint}${codiLinia}/estacions${apiAuth}`);
+  if (resp.ok) {
+    const paradas = await resp.json();
+    return paradas;
+  }
+};
+
+module.exports = {
+  obtenerLineas,
+  obtenerParadasLinea
+}
--- server.js
+++ server.js
@@ -1,9 +1,63 @@
+require("dotenv").config();
 const express = require("express");
 const chalk = require("chalk");
+const fetch = require("node-fetch");
+const morgan = require("morgan");
+const {obtenerLineas, obtenerParadasLinea} = require("./llamadasApi");
 
 const app = express();
 const puerto = process.env.PUERTO;
 
+let lineasMetro = null;
+
 app.listen(puerto, () =>
-  console.log(chalk.yellow("Servidor abierto en el puerto " + puerto))
+  console.log(chalk.yellow(`Servidor abierto en el puerto ${puerto}`))
 );
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("*", (req, res, next) => res.status(403).json({ error: true, mensaje: "Te pensabas que podías jaquearme" }));
+app.put("*", (req, res, next) => res.status(403).json({ error: true, mensaje: "Te pensabas que podías jaquearme" }));
+app.delete("*", (req, res, next) => res.status(403).json({ error: true, mensaje: "Te pensabas que podías jaquearme" }));
+
+app.get("/metro/lineas", async (req, res, next) => {
+  lineasMetro = await obtenerLineas();
+  const datosFormateados = lineasMetro.map((lineaMetro) => ({
+    id : lineaMetro.properties.CODI_LINIA,
+    linea : lineaMetro.properties.NOM_LINIA,
+    descripcion: lineaMetro.properties.DESC_LINIA
+  }));
+  res.json(datosFormateados);
+});
+
+app.get("/metro/linea/:nombreLinea", async (req, res, next) => {
+  const {nombreLinea} = req.params;
+  if (lineasMetro === null) {
+    lineasMetro = await obtenerLineas();
+  }
+  const lineaEncontrada = lineasMetro.find((linea) => linea.properties.NOM_LINIA.toLowerCase() === nombreLinea.toLowerCase());
+  if (!lineaEncontrada) {
+    res.json({ error: true, mensaje: "No existe la linea buscada"});
+  }
+  const {features: paradasLinea} = await obtenerParadasLinea(lineaEncontrada.properties.CODI_LINIA);
+  res.json(
+    {
+      linea: lineaEncontrada.properties.NOM_LINIA,
+      descripcion: lineaEncontrada.properties.DESC_LINIA,
+      paradas : paradasLinea.map(({properties: {ID_ESTACIO, NOM_ESTACIO}}) => ({
+        id: ID_ESTACIO,
+        nombre: NOM_ESTACIO
+      }))
+    }
+  );
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: true, mensaje: "Recurso no encontrado"});
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: true, mensaje: "Error general"});
+});
